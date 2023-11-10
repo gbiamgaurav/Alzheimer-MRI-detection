ce0ce38550503bec70c32a8250ba87ff22fae72b
Gaurav Bhattacharya <81230208+gbiamgaurav@users.noreply.github.com>
2023-11-10 10:25:23 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 
 from alzeihmer_detection import logger
 from alzeihmer_detection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from alzeihmer_detection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f"Stage: {STAGE_NAME} Started!")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f"Stage: {STAGE_NAME} Completed!")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model Stage"
+try:
+    logger.info(f"Stage: {STAGE_NAME} Started!")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f"Stage: {STAGE_NAME} Completed!")
 except Exception as e:
     logger.exception(e)
--- src/alzeihmer_detection/entity/config_entity.py
+++ src/alzeihmer_detection/entity/config_entity.py
@@ -10,3 +10,15 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path 
+    base_model_path: Path 
+    updated_base_model_path: Path 
+    params_image_size: list 
+    params_learning_rate: float
+    params_include_top: bool 
+    params_weights: str 
+    params_classes: int
+
+
